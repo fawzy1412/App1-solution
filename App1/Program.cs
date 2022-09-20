7cadce8ae4a4df2ee850e2c7cea2cf4829136eda
Administrator <Administrator@WIN-Q37T48CP7CA>
2022-09-20 11:11:02 +0200

    Add project files.

---
+++ App1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class helloWorld
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World, This is my first C# program");
+            Console.ReadKey();
+        }
+    }
+}
